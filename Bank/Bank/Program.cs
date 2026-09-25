83e23b498d559ac8ef414d82f37e8ea5d1a241ac
pantyhovAl <pantyvoh@gmail.com>
2026-09-25 15:13:36 +0700

    #1 test

--- Bank/Bank/Program.cs
+++ Bank/Bank/Program.cs
@@ -10,7 +10,7 @@
             Console.WriteLine($"{account2.Owner} {account2.Balance} {account2.Number}");
 
             account1.MakeDeposite(100000000000, DateTime.UtcNow, "");
-            Console.WriteLine($"{account1.Owner} {account1.Balance} {account1.Number}");
+            Console.WriteLine($" {account1.Owner} {account1.Balance} {account1.Number}");
             account1.MakeWithdrawl(100, DateTime.UtcNow, "");
             Console.WriteLine($"{account1.Owner} {account1.Balance} {account1.Number}");
 
